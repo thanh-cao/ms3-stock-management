84d87d550e3ef7420b0538f3987f80f46513ab0e
Thanh <kimthanh.cao@gmail.com>
2021-10-08 23:38:21 +0200

    build: sign up function

--- app.py
+++ app.py
@@ -2,20 +2,52 @@ import os
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
-# from flask_pymongo import PyMongo
+from flask_mongoengine import MongoEngine
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
+from models import *
+from forms import *
 if os.path.exists('env.py'):
     import env
 
-
+# set up Flask app and config
 app = Flask(__name__)
+app.config['MONGODB_SETTINGS'] = {
+    'db': os.environ.get('MONGO_DBNAME'),
+    'host': os.environ.get('MONGO_URI'),
+}
+app.secret_key = os.environ.get('SECRET_KEY')
+# set up Flask-Mongoengine connection to connect to MongoDB
+db = MongoEngine(app)
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = signupForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            existing_account = Account.objects(email=form.email.data).first()
+            if existing_account:
+                # flash("Email already registered!")
+                return redirect(url_for('signup'))
+
+            password = generate_password_hash(form.password.data)
+            admin_user = User(username=form.name.data,
+                            role='admin')
+            new_account = Account(name=form.name.data,
+                            email=form.email.data,
+                            password=password,
+                            company_name=form.company_name.data,
+                            user_access=admin_user)
+            new_account.save()
+            return redirect(url_for('index'))
+    return render_template('signup.html', form=form)
 
 
 if __name__ == '__main__':
---
+++ forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, validators, PasswordField, SubmitField
+from wtforms.validators import DataRequired, InputRequired, Email, Length
+import email_validator
+
+
+class signupForm(FlaskForm):
+    name = StringField(label='Name', validators=[DataRequired()])
+    email = StringField(label='Email', validators=[
+            DataRequired(), Email(granular_message=True)])
+    password = PasswordField('Password',
+        [InputRequired(message='You must provide a password!'),
+        Length(min=6, max=20, message="Your password must be between 6 and 20 characters long!")])
+    company_name = StringField(label='Company\'s Name')
+    submit = SubmitField(label="Sign Up")
