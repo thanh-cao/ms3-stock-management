a5289e4d01d2fb96c041f7be6738081702904350
Thanh <kimthanh.cao@gmail.com>
2021-10-09 01:30:33 +0200

    build: login function

--- app.py
+++ app.py
@@ -32,22 +32,57 @@ def signup():
     form = signupForm()
     if request.method == 'POST':
         if form.validate_on_submit():
+            # Check if account already exists in database
             existing_account = Account.objects(email=form.email.data).first()
             if existing_account:
-                # flash("Email already registered!")
+                flash("Email already registered!")
                 return redirect(url_for('signup'))
 
+            # Gather new account registration info and save to database
             password = generate_password_hash(form.password.data)
             admin_user = User(username=form.name.data,
-                            role='admin')
+                              pin=1010,
+                              role='admin')
             new_account = Account(name=form.name.data,
-                            email=form.email.data,
-                            password=password,
-                            company_name=form.company_name.data,
-                            user_access=admin_user)
+                                  email=form.email.data,
+                                  password=password,
+                                  company_name=form.company_name.data,
+                                  user_access=admin_user)
             new_account.save()
-            return redirect(url_for('index'))
+
+            session['account'] = form.name.data
+            return redirect(url_for('profile', name=session['account']))
     return render_template('signup.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = loginForm()
+    if request.method == 'POST':
+        # Check if account exists in database
+        existing_account = Account.objects(email=form.name.data).first()
+        password = form.password.data
+
+        if existing_account:
+            # Check if hashed password matches user input
+            if check_password_hash(existing_account['password'], password):
+                session['account'] = form.name.data
+                return redirect(url_for('profile', name=session['account']))
+            else:
+                # invalid password
+                flash('Incorrect Password')
+                return redirect(url_for('login'))
+        else:
+            # username doesn't exist
+            flash('Incorrect Username')
+            print('check password')
+            return redirect(url_for('login'))
+    return render_template('login.html', form=form)
+
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
 
 
 if __name__ == '__main__':
--- forms.py
+++ forms.py
@@ -7,9 +7,18 @@ import email_validator
 class signupForm(FlaskForm):
     name = StringField(label='Name', validators=[DataRequired()])
     email = StringField(label='Email', validators=[
-            DataRequired(), Email(granular_message=True)])
+        DataRequired(), Email(granular_message=True)])
     password = PasswordField('Password',
-        [InputRequired(message='You must provide a password!'),
-        Length(min=6, max=20, message="Your password must be between 6 and 20 characters long!")])
+                            [InputRequired(message='You must provide a password!'),
+                            Length(min=6, max=20, message='Your password must be between 6 and 20 characters long!')])
     company_name = StringField(label='Company\'s Name')
-    submit = SubmitField(label="Sign Up")
+    submit = SubmitField(label='Sign Up')
+
+
+class loginForm(FlaskForm):
+    email = StringField(label='Email', validators=[
+        DataRequired(), Email(granular_message=True)])
+    password = PasswordField('Password',
+                            [InputRequired(message='You must provide a password!'),
+                            Length(min=6, max=20, message='Your password must be between 6 and 20 characters long!')])
+    submit = SubmitField(label='Log In')
